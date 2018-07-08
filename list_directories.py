56e098778887298a26583fbc2d3f283d06539ab7
Tom Ritchford <tom@swirly.com>
2018-07-08 16:26:48 +0200

    More work

---
+++ list_directories.py
@@ -0,0 +1,19 @@
+import files, hasher, os, sys
+
+
+def size_and_header(root):
+    volumes, name = os.path.split(files.canonical_path(root))
+    assert volumes == '/Volumes', volumes
+
+    with open(name + '.txt', 'w') as fp:
+        for filename in files.walk(root):
+            try:
+                filesize, digest = hasher.hash_header_with_size(filename)
+                print(filename, filesize, digest, file=fp)
+            except:
+                print('FAILED', filename, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    for root in sys.argv[1:]:
+        size_and_header(root)
