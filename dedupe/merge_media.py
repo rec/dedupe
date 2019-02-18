91f3029cd1f469699e7d4fbfe6fca1ad2f4b0cfd
Tom Ritchford <tom@swirly.com>
2019-02-18 21:23:10 +0100

    Changes to merge_media

--- dedupe/merge_media.py
+++ dedupe/merge_media.py
@@ -1,43 +1,35 @@
 from . import files
-import mutagen, pathlib, shutil, sys
+import collections, mutagen, pathlib, shutil, sys
 
 SUFFIXES = {'.aif', '.aiff', '.m4a', '.mp3', '.wav', '.wave'}
 DELTA = 0.001
 DRY_RUN = True
 VERBOSE = True
-COPIES = REPLACEMENTS = IGNORED = TOTAL = 0
 
 
-def merge_all(source, target):
-    for s in pathlib.Path(source).glob('*'):
-        if not s.stem.startswith('.'):
-            merge_one(source, target)
-    print(COPIES, 'copies')
-    print(REPLACEMENTS, 'replacements')
-    print(IGNORED, 'ignored')
-    print(COPIES + REPLACEMENTS + IGNORED, 'total')
-    print(TOTAL, 'check total')
-
-
-def merge_one(source, target):
+def merge_one(source, target, counter):
     source = pathlib.Path(source)
+
+    if VERBOSE:
+        print('Merging  ', source, '->', target)
+
     for sfile in files.walk(source):
         if sfile.suffix.lower() in SUFFIXES:
-            copy(sfile, target / sfile.relative_to(source))
+            copy(sfile, target / sfile.relative_to(source), counter)
 
 
-def copy(sfile, tfile):
+def copy(sfile, tfile, counter):
     def do_copy(replace):
         if VERBOSE:
-            print('Replacing' if replace else 'Copying', sfile, '->', tfile)
+            action = 'Replacing' if replace else 'Copying  '
+            print(action, sfile, '->', tfile)
 
         if not DRY_RUN:
             if not replace:
                 tfile.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy(sfile, tfile)
 
-    global IGNORED, TOTAL, COPIES, REPLACEMENTS
-    TOTAL += 1
+    counter['total'] += 1
 
     while tfile.exists():
         slength = mutagen.File(sfile).info.length
@@ -48,11 +40,10 @@ def copy(sfile, tfile):
             # we assume they're the same, and we take the biggest file, hoping
             # that's always the highest quality
             if sfile.stat.st_size > tfile.stat.st_size:
-                # On the theory bigger is better
                 do_copy(True)
-                REPLACEMENTS += 1
+                counter['replacements'] += 1
             else:
-                IGNORED += 1
+                counter['ignored'] += 1
             return
 
         # source and target are actually different: rename and try again
@@ -63,8 +54,27 @@ def copy(sfile, tfile):
             rest += (last, '1')
         tfile = tfile.parent / (' '.join(rest) + tfile.suffix)
 
-    COPIES += 1
+    counter['copies'] += 1
     do_copy(False)
+
+
+
+def merge_all(source, target, counter):
+    for s in pathlib.Path(source).glob('*'):
+        if not s.stem.startswith('.'):
+            merge_one(source, target, counter)
+    report(counter)
+
+
+def report(counter):
+    total = 0
+    for name in 'copies', 'replacements', 'ignored':
+        value = counter[name]
+        print(value, name)
+        total += value
+
+    print(counter['total'], 'total')
+    print(total, 'check total')
 
 
 if __name__ == '__main__':
