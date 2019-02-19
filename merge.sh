61ba73a935bf6ed4ad0adb2ddd218dc3067ed6bc
Tom Ritchford <tom@swirly.com>
2019-02-19 11:19:21 +0100

    Add emacs backups to .gitignore

---
+++ merge.sh
@@ -0,0 +1,11 @@
+ROOT=/Volumes/McLuhan/Media
+
+python3 -m dedupe.merge_media \
+ /Volumes/Matmos/Media \
+ "$ROOT/Buckminster" \
+ "$ROOT/Deutschland" \
+ "$ROOT/Newton" \
+ "$ROOT/Rachel\'s Mac Pro" \
+ "$ROOT/bantam" \
+ "$ROOT/bantam 2" \
+ "$ROOT/bantam 3" 
