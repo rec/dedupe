99bd1b9c4934fdfbc17d6979b6a8361c0f18e951
Tom Ritchford <tom@swirly.com>
2019-02-24 19:54:10 +0100

    find_case_issues

--- dedupe/find_case_issues.py
+++ dedupe/find_case_issues.py
@@ -1,15 +1,19 @@
-import pprint
+import json, pathlib, sys
 from . import merge_media
 
 
-def find_case_issues(root):
+def find_case_issues(source):
     files = {}
     for sfile in merge_media.walk(source):
+        sfile = sfile.relative_to(source)
         if sfile.suffix.lower() in merge_media.SUFFIXES:
             files.setdefault(str(sfile).lower(), set()).add(str(sfile))
 
-    return {k: v for k, v in files.items() if len(v) > 1}
-
+    for k, v in sorted(files.items()):
+        if len(v) > 1:
+            print(*sorted(v), sep='\n')
+            print()
+    
 
 if __name__ == '__main__':
-    pprint.pprint(find_case_issues(sys.argv[1]))
+    issues = find_case_issues(sys.argv[1])
