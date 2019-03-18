48ee33eb5fc34cdb037dfb9c41d6835e82e97cb2
Tom Ritchford <tom@swirly.com>
2019-03-18 20:21:34 +0100

    Improved merge_photos

--- dedupe/merge_photos.py
+++ dedupe/merge_photos.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from . import merge_media
 
 SUFFIXES = '.avi', '.jpg', '.mov', '.mp4', '.tif','.gif'
-
+DRY_RUN = True
 
 def collect_all_suffixes(root):
     suffixes = set()
@@ -17,11 +17,31 @@ def collect_all_suffixes(root):
 
 def walk(source, suffixes=SUFFIXES):
     for dirpath, dirs, filenames in os.walk(source):
-        dirpath = Path(dirpath)
         for filename in filenames:
             path = Path(filename)
             if path.suffix.lower() in suffixes:
-                yield path
+                yield dirpath / path
+
+
+def merge(source, target):
+    for source_file in walk(source):
+        rel = source_file.relative_to(source)
+        target_file = target / rel
+        if not  target_file.exists:
+            if DRY_RUN:
+                print('Moving', rel)
+            else:
+                _move(source_file, target_file)
+
+
+def _move(source, target):
+    target.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        shutil.move(source, target)
+    except:
+        print('FAILED to move', source, file=sys.err)
+    else:
+        print('Moved', source)
 
 
 if __name__ == '__main__':
