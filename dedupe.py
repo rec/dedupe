33761ffdb19088d93651a9633a3a87592c033c05
Tom Ritchford <tom@swirly.com>
2020-07-28 20:33:39 +0200

    Start extracting data

--- dedupe.py
+++ dedupe.py
@@ -5,23 +5,35 @@ import sys
 
 
 def accept_dir(d):
-    return not (d.startswith('.') or d == '__pycache__' or d in IGNORE_DIRS)
+    return not (
+        d.startswith('.')
+        or d == '__pycache__'
+        or d in IGNORE_DIRS
+        or d.endswith('.app')
+    )
 
 
 def accept_file(d):
-    return not d.endswith('.pyc')
+    return not (d.endswith('.pyc') or d in IGNORE_FILES)
 
 
 def run_one(root):
     for dirpath, dirnames, filenames in os.walk(root):
-       if dirpath == root:
-           dirnames[:] = [d for d in dirnames if d not in IGNORE_OS_DIRECTORIES]
-       dirnames[:] = sorted(d for d in dirnames if accept_dir(d))
-       for filename in sorted(f for f in filenames if accept_file(f)):
-           path = '%s/%s' % (dirpath, filename)
-           st = os.stat(path)
-           record = {'mtime': st.st_mtime, 'size': st.st_size, 'path': path}
-           print(json.dumps(record))
+        if dirpath == root:
+            dirnames[:] = [
+                d for d in dirnames if d not in IGNORE_OS_DIRECTORIES
+            ]
+        dirnames[:] = sorted(d for d in dirnames if accept_dir(d))
+        for filename in sorted(f for f in filenames if accept_file(f)):
+            path = '%s/%s' % (dirpath, filename)
+            try:
+                st = os.stat(path)
+            except Exception:
+                print('Skipping', path, file=sys.stderr)
+                continue
+
+            record = {'mtime': st.st_mtime, 'size': st.st_size, 'path': path}
+            print(json.dumps(record))
 
 
 def main(args=None):
